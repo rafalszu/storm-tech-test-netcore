29ca66fff1e33f1ab237da2d66f50eabc8ee8360
Rafal <rafal@szumilas.org>
2020-02-25 14:35:25 +0100

    task 7: orderByRank tests

--- Todo.Tests/TodoListControllerTests.cs
+++ Todo.Tests/TodoListControllerTests.cs
@@ -40,7 +40,7 @@ namespace Todo.Tests
                 );
                 
             TodoListController controller = new TodoListController(null, applicationDbContextMock.Object, userStoreMock.Object);
-            IActionResult actual = controller.Detail(1, hide);
+            IActionResult actual = controller.Detail(1, hide, null);
 
             ViewResult viewResult = actual as ViewResult;
             viewResult
@@ -64,6 +64,63 @@ namespace Todo.Tests
             viewmodel.Items
                 .Should()
                 .HaveCount(expectedCount);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void DetailTakesOrderByRankIntoAccount(bool? orderByRank)
+        {
+            var todoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                    .WithItem("bread", Importance.High)
+                    .WithItem("milk", Importance.Low)
+                    .WithItem("eggs", Importance.Medium)
+                    .Build();
+
+            // set rank on items
+            todoList.Items.ElementAt(0).Rank = 2;
+            todoList.Items.ElementAt(1).Rank = 1;
+            todoList.Items.ElementAt(2).Rank = 3;
+
+            applicationDbContextMock
+                .Setup(x => 
+                    x.SingleTodoList(It.IsAny<int>())
+                ).Returns(
+                    todoList
+                );
+                
+            TodoListController controller = new TodoListController(null, applicationDbContextMock.Object, userStoreMock.Object);
+            IActionResult actual = controller.Detail(1, null, orderByRank);
+
+            ViewResult viewResult = actual as ViewResult;
+            viewResult
+                .Should()
+                .NotBeNull();
+
+            viewResult.Model
+                .Should()
+                .BeOfType<TodoListDetailViewmodel>();
+
+            var viewmodel = viewResult.Model as TodoListDetailViewmodel;
+
+            viewmodel.Items
+                .Should()
+                .NotBeNull();
+            
+            string expectedFirstItemTitle = "bread"; // beacuse by default this should be sorted by importance
+            if(orderByRank == true)
+                expectedFirstItemTitle = "milk";
+
+            viewmodel.Items
+                .Should()
+                .HaveCount(3);
+
+            viewmodel.Items
+                .ElementAt(0)
+                .Title
+                .Should()
+                .BeSameAs(expectedFirstItemTitle);
         }
     }
 }
